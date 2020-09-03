1b69de329cccbf18b1f97010620b10d8c097d130
anurudh <(none)>
2020-09-03 05:44:53 +0530

    Update quantum wrappers

--- tools/mlir-quantum-simulator/mlir-quantum-simulator.cpp
+++ tools/mlir-quantum-simulator/mlir-quantum-simulator.cpp
@@ -1,6 +1,29 @@
-#include <iostream>
+//===- mlir-quantum-simulator.cpp - MLIR Quantum Simulator ----------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Main entry point to a command line utility that executes an MLIR file on the
+// CPU by translating MLIR to LLVM IR before JIT-compiling and executing the
+// latter.
+//
+//===----------------------------------------------------------------------===//
 
-int main(int argc, char *argv[]) {
-  std::cout << "[INCOMPLETE] Quantum Simulator Driver" << std::endl;
-  return 0;
+#include "mlir/ExecutionEngine/JitRunner.h"
+#include "mlir/ExecutionEngine/OptUtils.h"
+#include "mlir/InitAllDialects.h"
+#include "llvm/Support/InitLLVM.h"
+#include "llvm/Support/TargetSelect.h"
+
+int main(int argc, char **argv) {
+  mlir::registerAllDialects();
+  llvm::InitLLVM y(argc, argv);
+  llvm::InitializeNativeTarget();
+  llvm::InitializeNativeTargetAsmPrinter();
+  mlir::initializeLLVMPasses();
+
+  return mlir::JitRunnerMain(argc, argv, nullptr);
 }
--- tools/mlir-quantum-simulator/quantum-simulator-wrappers.cpp
+++ tools/mlir-quantum-simulator/quantum-simulator-wrappers.cpp
@@ -1,18 +1,49 @@
+//===- quantum-simulator-wrappers.cpp - Quantum Dialect Simulation --------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Wrapper functions to execute operations from the quantum dialect.
+// The quantum program will effectively be simulated using PRGs
+// for measurements
+//
+//===----------------------------------------------------------------------===//
+
 #include <iostream>
+#include <vector>
 using namespace std;
 
-class Qubit {
+using QubitSlice = vector<int>;
+
+class QubitRegister {
+  unsigned numQubits;
+  unsigned long long size;
+  vector<double> state;
+  vector<int> unused;
 public:
-  Qubit(int n) {
-    cerr << "> {allocate} " << n << endl;
+  QubitRegister(int numQubits) :
+    numQubits(numQubits),
+    size(1ll << numQubits),
+    state(size, 0.0) {
+    state[0] = 1.0;
+  }
+
+  // measures and releases qubits
+  vector<bool> measure(const QubitSlice& idx) {
+    return {};
   }
 };
 
-using QubitPtr = void*;
 
-extern "C" QubitPtr acquire_qubits(int size) {
-  Qubit *qs = new Qubit(size);
-  return (void*) qs;
+extern "C" void* acquire_qubits(int size) {
+  return nullptr;
+}
+
+extern "C" int* measure(void* qs) {
+  return nullptr;
 }
 
 extern "C" void printLn(int n) {
