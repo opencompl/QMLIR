60274b25a2533fbbee0b651fe3e53438a78f2624
anurudhp <anurudhp97@gmail.com>
2020-12-11 02:25:00 +0530

    [WIP] refactor

---
+++ include/Dialect/QASM/QASMDialect.h
@@ -0,0 +1,16 @@
+//===- QASMDialect.h - QASM dialect ------------ ----------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef QASM_QASMDIALECT_H
+#define QASM_QASMDIALECT_H
+
+#include "mlir/IR/Dialect.h"
+
+#include "Dialect/QASM/QASMDialect.h.inc"
+
+#endif // QASM_QASMDIALECT_H
---
+++ include/Dialect/QASM/QASMTypes.h
@@ -0,0 +1,12 @@
+#ifndef QASM_QASMTYPES_H
+#define QASM_QASMTYPES_H
+
+#include "mlir/IR/StandardTypes.h"
+#include "mlir/IR/TypeSupport.h"
+#include "mlir/IR/Types.h"
+
+namespace mlir {
+namespace QASM {} // namespace QASM
+} // namespace mlir
+
+#endif // QASM_QASMTYPES_H
