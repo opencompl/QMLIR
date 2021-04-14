e3824bed9cc043567b3146657f3b54251d664cff
anurudhp <anurudhp97@gmail.com>
2021-04-14 19:45:40 +0530

    [optimization] add dummy opt for merging consecutive U. The actual algo is too complicated, will implement it later. Right now it just removes the root op, replacing with the parent.

--- lib/Dialect/Quantum/Transforms/QuantumRewrites.cpp
+++ lib/Dialect/Quantum/Transforms/QuantumRewrites.cpp
@@ -20,9 +20,42 @@ namespace {
 #include "Dialect/Quantum/Transforms/QuantumRewrites.h.inc"
 } // namespace
 
+struct EulerAngles {
+  double theta, phi, lambd;
+  static EulerAngles fromYZY(double theta, double phi, double lambd) {
+    /// Dummy implementation for testing
+    /// TODO: implement actual algorithm
+    return {theta, phi, lambd};
+  }
+};
+
+// U(theta, phi, lambda)
+//           = U(theta2, phi2, lambda2).U(theta1, phi1, lambda1)
+//           = Rz(phi2).Ry(theta2).Rz(lambda2+phi1).Ry(theta1).Rz(lambda1)
+//           = Rz(phi2).Rz(phi').Ry(theta').Rz(lambda').Rz(lambda1)
+//           = U(theta', phi2 + phi', lambda1 + lambda')
+class UOpMergePattern : public OpRewritePattern<UniversalRotationGateOp> {
+public:
+  using OpRewritePattern::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(UniversalRotationGateOp uOp,
+                                PatternRewriter &rewriter) const final {
+    auto parentUOp = uOp.qinp().getDefiningOp<UniversalRotationGateOp>();
+    if (!parentUOp)
+      return failure();
+
+    /// Dummy implementation for testing only
+    /// TODO: implement proper merge: U.U
+    rewriter.replaceOp(uOp, parentUOp.qout());
+
+    return success();
+  }
+};
+
 void QuantumRewritePass::runOnFunction() {
   OwningRewritePatternList patterns(&getContext());
   populateWithGenerated(patterns);
+  patterns.insert<UOpMergePattern>(&getContext());
   if (failed(
           applyPatternsAndFoldGreedily(getFunction(), std::move(patterns)))) {
     signalPassFailure();
