6350d5e2f611c56488d6f17695a18777a40a57af
anurudhp <anurudhp97@gmail.com>
2022-02-06 17:07:41 +0530

    fix typo

--- lib/Dialect/QASM/Transforms/MakeGatesTransparent.cpp
+++ lib/Dialect/QASM/Transforms/MakeGatesTransparent.cpp
@@ -36,7 +36,7 @@ public:
   }
 };
 
-class GatePass : public QASMMakeGatesOpaquePassBase<GatePass> {
+class GatePass : public QASMMakeGatesTransparentPassBase<GatePass> {
   void runOnFunction() override;
 };
 
